fc20a51754052c3096bb40d9c4dffb2746a89aaa
Jiwon Kim <kms602700@naver.com>
2021-06-01 20:47:30 +0900

    Add files via upload

--- app2.R
+++ app2.R
@@ -2,7 +2,7 @@ library(shiny)
 library(data.table)
 library(leaflet)
 
-long_dat<-read.csv("long_dat.csv")
+long_dat<-read.csv("C:/Users/kms60/OneDrive/shiny/long_dat.csv")
 
 pal<-colorFactor("viridis", long_dat$type)
 
